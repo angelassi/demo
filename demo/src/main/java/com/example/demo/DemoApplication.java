fb1702b178a658c18e7dd77ff80e80d9c9a3f532
라시100 <100@DESKTOP-TA1CSU8>
2023-08-12 19:36:04 +0900

    1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
+
+	@RequestMapping("/")
+	String a() {
+		return "Hello Word";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
